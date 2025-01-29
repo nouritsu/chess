30694372fddf31361d839fc978dc661865b0181a
Aneesh <aneesh1701@gmail.com>
2025-01-29 11:48:34 +0100

    split app into multiple files

---
+++ src/app.rs
@@ -0,0 +1,37 @@
+use crate::{board, board_position, cursor};
+use bevy::{prelude::*, window::CursorOptions};
+
+pub const WINDOW_WIDTH: f32 = 512.;
+pub const WINDOW_HEIGHT: f32 = 512.;
+
+#[derive(Component)]
+pub struct MainCamera;
+
+fn setup(mut cmd: Commands) {
+    cmd.spawn((Camera2d::default(), MainCamera));
+}
+
+pub fn plugin(app: &mut App) {
+    let window = Window {
+        title: "Chess".to_owned(),
+        resolution: (WINDOW_WIDTH, WINDOW_HEIGHT).into(),
+        cursor_options: CursorOptions {
+            visible: false,
+            ..Default::default()
+        },
+        ..default()
+    };
+
+    let default_plugins = DefaultPlugins.set(WindowPlugin {
+        primary_window: Some(window),
+        ..default()
+    });
+
+    app.add_plugins((
+        default_plugins,
+        board::plugin,
+        cursor::plugin,
+        board_position::plugin,
+    ))
+    .add_systems(Startup, setup);
+}
